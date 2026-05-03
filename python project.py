88ecea5766742914787e2c086496587821105186
elijahrkhennigan-source <elijahrkhennigan@gmail.com>
2026-05-03 13:41:22 -0500

    Add files via upload

---
+++ python project.py
@@ -0,0 +1,37 @@
+import random
+
+lowest_num = 1
+highest_num = 1000
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
+
+print("Number guessing game")
+
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+
+    guess = input("enter guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("number not in range")
+            print (f"Please select a number between {lowest_num} and {highest_num}")
+
+        elif guess < answer:
+            print("Too low! Try again!")
+        elif guess > answer:
+            print("Too high! Try again!")
+        else:
+            print(f"CORRECT! The answer was {answer}")
+
+            print(f"Number of guesses: {guesses}")
+            is_running = False
+
+    else:
+        print("invalid")
+        print (f"Please select a number between {lowest_num} and {highest_num}")
